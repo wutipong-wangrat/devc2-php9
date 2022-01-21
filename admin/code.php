d4ed724d2e2e02bfbe3e6c2ced5cd3c4677c7b38
wutipong wangrat <bigbosszaa086@gmail.com>
2022-01-21 10:34:00 +0700

    add

---
+++ admin/code.php
@@ -0,0 +1,6 @@
+<?php 
+if(isset($_POST['login_btn'])){
+    echo $email_login = $_POST['email'];
+    echo $password_login = $_POST['password'];
+}
+?>
---
+++ admin/login.php
@@ -0,0 +1,41 @@
+<?php include("includes/header.php"); ?>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow my-5">
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="col-lg-12">
+
+                            <div class="card-text mx-auto" style="width: 20rem;">
+                                <img src="img/thanos1.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h3 class="card-title text-center text-danger fs-2">Phalakorn System</h3>
+                                </div>
+                            </div>
+
+                            <form action="code.php" method="post">
+                                <div class="form-group">
+                                    <label class="col-sm-3 col-form-label text-primary">อีเมลล์</label>
+                                    <input type="email" name="email" class="form-control" required placeholder="Enter Email Address">
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-3 col-form-label text-primary">รหัสผ่าน</label>
+                                    <input type="password" name="password" class="form-control" required placeholder="Enter Password">
+                                </div>
+                                <button type="submit" name="login_btn" class="btn btn-primary btn-block">Login</button>
+                            </form>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php"); ?>
+<?php include("includes/script.php"); ?>
