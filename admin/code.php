6dcd3ee82e4373cea9e5e1a614d63cc91e829e90
wutipong wangrat <bigbosszaa086@gmail.com>
2022-01-21 11:06:24 +0700

    git commit -m "initial commit"

--- admin/code.php
+++ admin/code.php
@@ -1,6 +1,18 @@
+<?php include("includes/dbconnect.php");?>
+
 <?php 
 if(isset($_POST['login_btn'])){
-    echo $email_login = $_POST['email'];
-    echo $password_login = $_POST['password'];
+    $email_login = $_POST['email'];
+    $password_login = $_POST['password'];
+
+    $query="SELECT * from users WHERE email='$email_login' AND password='$password_login' ";
+    $query_run=mysqli_query($conn,$query);
+
+    if(mysqli_fetch_array($query_run)){
+        echo"OK";
+    }
+    else{
+        echo"NO";
+    }
 }
 ?>
---
+++ admin/includes/dbconnect.php
@@ -0,0 +1,9 @@
+<?php 
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbconnect = "cirdb";
+
+    $conn=mysqli_connect("$host","$user","$password","cirdb");
+    // $conn=mysqli_connect("localhost","root","","cirdb");
+?>
